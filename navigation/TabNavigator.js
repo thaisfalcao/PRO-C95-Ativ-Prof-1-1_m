18b733afa2fd67343c1f163d4d5c7aae8896bee9
Thaís Falcão <101199332+thaisfalcao@users.noreply.github.com>
2023-03-07 12:05:03 -0300

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from "../screens/Feed";
+import Home from "../screens/Home";
+
